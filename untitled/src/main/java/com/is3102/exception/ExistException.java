d0a0dc78d36d6501019640ba610da8c25b3518ea
Bryan Arnold <bryan_ahc@hotmail.com>
2013-09-23 23:17:41 +0800

    made changes to doctor entity sheduling and resource allocation: remote interface and bean scheduling and resource allocation managed bean added

--- untitled/src/main/java/com/is3102/exception/ExistException.java
+++ untitled/src/main/java/com/is3102/exception/ExistException.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.is3102.Exception;
+package com.is3102.exception;
 
 import javax.ejb.ApplicationException;
 /**
