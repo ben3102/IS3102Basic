c776d7bb66947dad1a9eb8222ffd0f245e95e169
user <user@user-PC>
2013-09-26 18:58:01 +0800

    Swarit's changes

---
+++ untitled/src/main/java/com/is3102/Exception/ExistException.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.is3102.Exception;
+
+import javax.ejb.ApplicationException;
+/**
+ *
+ * @author Ben
+ */
+@ApplicationException(rollback=true)
+public class ExistException extends Exception{
+    public ExistException(){}
+    public ExistException(String message){
+        super(message);
+    }
+}
